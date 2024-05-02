6269d67b71ae5f9b581bf543778c67a9d7a88dc4
Davi Nascimento <galaxys3tab32@gmail.com>
2024-05-02 07:44:30 +0000

    update de arquivos e create de campos `plate` e `photo` na models.py

--- project_cars/cars/urls.py
+++ project_cars/cars/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import cars
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
   path('', cars),
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) # para configurar uso de armazenar imagens
+
--- project_cars/core/settings.py
+++ project_cars/core/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,3 +119,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# CONFIGURAÇÂO PARA USAR IMAGENS
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
