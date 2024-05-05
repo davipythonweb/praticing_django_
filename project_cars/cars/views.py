e0eff1d66b8e9aa5d6a4580ac0be0f516f01b32f
Davi Nascimento <galaxys3tab32@gmail.com>
2024-05-05 04:02:02 +0000

    update `views.py` do app cars

--- project_cars/cars/views.py
+++ project_cars/cars/views.py
@@ -5,5 +5,12 @@ from cars.models import Car
 
 @csrf_protect
 def cars(request):
-  cars = Car.objects.all()
+  # cars = Car.objects.all() # pegando todos os dados
+  # cars = Car.objects.filter(brand__name='Fiat') # filtrando os dados usando o __ + o campo, navegando entre tabelas.
+  cars = Car.objects.filter(brand='2') # filtrando os dados
+  cars = Car.objects.filter(model='Chevette tubarão') # filtrando pelo modelo
+  cars = Car.objects.filter(model__contains='Chevette tubarão') # filtrando pelo modelo
+  
+
+
   return render(request,'cars.html', {'cars': cars})
