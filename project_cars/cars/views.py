4e4f4821b9c7a06648e0e126725eede70ff96a7a
Davi Nascimento <galaxys3tab32@gmail.com>
2024-05-05 09:07:01 +0000

    update `viwes.py` do app cars

--- project_cars/cars/views.py
+++ project_cars/cars/views.py
@@ -7,10 +7,10 @@ from cars.models import Car
 def cars(request):
   # cars = Car.objects.all() # pegando todos os dados
   # cars = Car.objects.filter(brand__name='Fiat') # filtrando os dados usando o __ + o campo, navegando entre tabelas.
-  cars = Car.objects.filter(brand='2') # filtrando os dados
-  cars = Car.objects.filter(model='Chevette tubarão') # filtrando pelo modelo
-  cars = Car.objects.filter(model__contains='Chevette tubarão') # filtrando pelo modelo
-  
+  # cars = Car.objects.filter(brand='2') # filtrando os dados pelo id
+  # cars = Car.objects.filter(model='Chevette tubarão') # filtrando pelo modelo
+  cars = Car.objects.filter(model__contains='C') # filtrando pela função (__contains) para pegar qualquer campo que comece com a string passada
+
 
 
   return render(request,'cars.html', {'cars': cars})
