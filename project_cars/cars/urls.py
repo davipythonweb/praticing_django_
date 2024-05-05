bffbb2131165e82e03c4241861e8b72e56787a68
Davi Nascimento <galaxys3tab32@gmail.com>
2024-05-05 16:05:22 +0000

    update `urls.py` do app cars

--- project_cars/cars/urls.py
+++ project_cars/cars/urls.py
@@ -3,6 +3,7 @@ from .views import cars
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
   path('', cars),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) # para configurar uso de armazenar
