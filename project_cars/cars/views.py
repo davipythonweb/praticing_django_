f4da5e53bf75ebd8883a8ce08f3862c3950707e6
davipythonweb <galaxys3tab32@gmail.com>
2024-05-16 05:30:57 -0300

    update rota com function `reverse_lazy` do django

--- project_cars/cars/views.py
+++ project_cars/cars/views.py
@@ -5,7 +5,7 @@ from cars.models import Car
 from cars.forms import CarModelForm
 # from django.views import View
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
-
+from django.urls import reverse_lazy
 
 
 # usando Function Based views(CODIGO AMADOR)
@@ -121,7 +121,10 @@ class CarUpdateView(UpdateView):
   model = Car
   form_class = CarModelForm
   template_name = 'car_update.html'
-  success_url = '/'
+  
+  def get_success_url(self):
+    return reverse_lazy('car_detail', kwargs={'pk': self.object.pk})
+  
   
 class CarDeleteView(DeleteView):
   model = Car
