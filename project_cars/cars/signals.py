753be17659349c6f8904ee7d7a00adbe23409e9e
davipythonweb <galaxys3tab32@gmail.com>
2024-05-20 19:51:12 -0300

    create function `carlula estoque ` `calcula valor total dos carros em estoque`

--- project_cars/cars/signals.py
+++ project_cars/cars/signals.py
@@ -1,31 +1,21 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from cars.models import Car
-# from django.db.models.signals import pre_save, pre_delete
+from cars.models import Car, CarInventory
+
+from django.db.models import Sum
 
 
-
-# @receiver(pre_save, sender=Car)
-# def car_pre_save(sender, instance, **kwargs):
-#     print('### PRE SAVE ###')
-#     print(instance)
-
+def car_invetory_update():
+    cars_count = Car.objects.all().count() # calcula a qtd de carros no estoque
+    cars_value = Car.objects.aggregate(total_value=Sum('value'))['total_value'] # calcula o o valor total da soma(R$) dos carros do estoque
+    CarInventory.objects.create(cars_count=cars_count, cars_value=cars_value) # cria a nova tabela no banco
+    
     
 @receiver(post_save, sender=Car)
 def car_post_save(sender, instance, **kwargs):
-    cars_count = Car.objects.all().count()
-    
-    
-    
-
-# @receiver(pre_delete, sender=Car)
-# def car_pre_delete(sender, instance, **kwargs):
-#     print('### PRE DELETE ###')
-#     print(instance)
+    car_invetory_update()
     
 @receiver(post_delete, sender=Car)
 def car_post_delete(sender, instance, **kwargs):
-    print('### POST DELETE ###')
-    print(instance)
-    
+    car_invetory_update()
     
