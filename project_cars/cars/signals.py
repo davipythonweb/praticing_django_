05c146de27fefe2bba7b79050589bf904209cc5f
davipythonweb <galaxys3tab32@gmail.com>
2024-05-20 18:53:16 -0300

    create function `car_post_save`

--- project_cars/cars/signals.py
+++ project_cars/cars/signals.py
@@ -1,25 +1,27 @@
-from django.db.models.signals import pre_save, pre_delete, post_save, post_delete
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from cars.models import Car
+# from django.db.models.signals import pre_save, pre_delete
 
 
-@receiver(pre_save, sender=Car)
-def car_pre_save(sender, instance, **kwargs):
-    print('### PRE SAVE ###')
-    print(instance)
+
+# @receiver(pre_save, sender=Car)
+# def car_pre_save(sender, instance, **kwargs):
+#     print('### PRE SAVE ###')
+#     print(instance)
+
     
 @receiver(post_save, sender=Car)
 def car_post_save(sender, instance, **kwargs):
-    print('### POST SAVE ###')
-    print(instance)
+    cars_count = Car.objects.all().count()
     
     
     
 
-@receiver(pre_delete, sender=Car)
-def car_pre_delete(sender, instance, **kwargs):
-    print('### PRE DELETE ###')
-    print(instance)
+# @receiver(pre_delete, sender=Car)
+# def car_pre_delete(sender, instance, **kwargs):
+#     print('### PRE DELETE ###')
+#     print(instance)
     
 @receiver(post_delete, sender=Car)
 def car_post_delete(sender, instance, **kwargs):
