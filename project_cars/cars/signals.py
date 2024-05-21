890de68aacb44e7859f7f3c7f6a6463f166adfb3
davipythonweb <galaxys3tab32@gmail.com>
2024-05-20 22:42:30 -0300

    creation function  pre_save `bio`

--- project_cars/cars/signals.py
+++ project_cars/cars/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import pre_save, post_save, post_delete
 from django.dispatch import receiver
 from cars.models import Car, CarInventory
 
@@ -11,6 +11,11 @@ def car_invetory_update():
     CarInventory.objects.create(cars_count=cars_count, cars_value=cars_value) # cria a nova tabela no banco
     
     
+@receiver(pre_save, sender=Car)
+def car_pre_save(sender, instance, **kwargs):
+   if not instance.bio:
+       instance.bio = 'Bio gerada automaticamente!'
+        
 @receiver(post_save, sender=Car)
 def car_post_save(sender, instance, **kwargs):
     car_invetory_update()
