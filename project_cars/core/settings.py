123429a45599669e298fe086ad8cd6dbaf5bff91
davipythonweb <galaxys3tab32@gmail.com>
2024-05-24 14:20:50 -0300

    update `settings.py`

--- project_cars/core/settings.py
+++ project_cars/core/settings.py
@@ -89,6 +89,19 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
+# conexao para sqlite3
+"""
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+"""
+
+# conexao para postgreSQL
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
