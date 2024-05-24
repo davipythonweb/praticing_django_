7579a93ffb7b23751f8bacd3985fb1f72114ab20
davipythonweb <galaxys3tab32@gmail.com>
2024-05-24 12:02:04 -0300

    update `settings.py`

--- project_cars/core/settings.py
+++ project_cars/core/settings.py
@@ -94,9 +94,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('NAME_DB'),
         'USER': os.environ.get('USER_DB'),
-        'PASSWORD': os.environ.getnv('PASSWORD_DB'),
-        'HOST': os.environ.getnv('HOST_DB'),
-        'PORT': os.environ.getnv('PORT_DB'),
+        'PASSWORD': os.environ.get('PASSWORD_DB'),
+        'HOST': os.environ.get('HOST_DB'),
+        'PORT': os.environ.get('PORT_DB'),
     }
 }
 
